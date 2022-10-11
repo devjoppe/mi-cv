4221619af3f78835b5bd69ba1f7a4efffa239e2d
devjoppe <joakim.ottosson@medieinstitutet.se>
2022-10-11 10:53:59 +0200

    Responsive aside component done in the first stage.

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,15 @@ menuBtn.addEventListener("click", function() {
     menuBtn.classList.remove("is--clicked");
   }, 300);
 });
+
+var aboutMeBtn = document.querySelector(".aboutme");
+var aboutMe = document.querySelector(".aside");
+var closeAsideBtn = document.querySelector(".close-aside");
+
+aboutMeBtn.addEventListener("click", function() {
+  aboutMe.classList.toggle("aside-active");
+});
+
+closeAsideBtn.addEventListener("click", function() {
+  aboutMe.classList.toggle("aside-active");
+});
