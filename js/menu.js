568860f8d06e7464b6748dbe0cb15dc04848f502
devjoppe <joakim.ottosson@medieinstitutet.se>
2022-10-13 13:47:12 +0200

    functions for opening more positions

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+// Menu
 var menuBtn = document.querySelector(".menu-btn");
 var myMenu = document.querySelector(".close");
 
@@ -10,6 +11,7 @@ menuBtn.addEventListener("click", function() {
   }, 300);
 });
 
+// About me
 var aboutMeBtn = document.querySelector(".aboutme");
 var aboutMe = document.querySelector(".aside");
 var closeAsideBtn = document.querySelector(".close-aside");
@@ -20,4 +22,20 @@ aboutMeBtn.addEventListener("click", function() {
 
 closeAsideBtn.addEventListener("click", function() {
   aboutMe.classList.toggle("aside-active");
+});
+
+// Experience expand
+var expandListBtn = document.querySelector(".expand-exp");
+var expandList = document.querySelector(".expand-list");
+var closelinkBtn = 'Close ' + '<i class="fa-sharp fa-solid fa-arrow-right">';
+var openlinkBtn = 'See 4 more positions ' + '<i class="fa-sharp fa-solid fa-arrow-right">';
+
+expandListBtn.addEventListener("click", function() {
+  expandList.classList.toggle("expand-list-toggle");
+  if(expandList.classList.contains("expand-list-toggle")) {
+    console.log("hejsan");
+    document.querySelector(".expand-link").innerHTML = closelinkBtn;
+  } else {
+    document.querySelector(".expand-link").innerHTML = openlinkBtn;
+  }
 });
